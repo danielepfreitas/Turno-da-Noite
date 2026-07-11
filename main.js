1446e78aafefb1c05742d52d09e43357c8d78f80
danielepfreitas <b8vjn6t7wz@privaterelay.appleid.com>
2026-07-11 12:35:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+const start = document.querySelector("#start");
+const intro = document.querySelector("#intro");
+const scene = document.querySelector("#scene");
 
+start.addEventListener("click", iniciar);
+
+function iniciar(){
+
+intro.style.opacity="0";
+
+setTimeout(()=>{
+
+scene.innerHTML=`
+
+<div id="opening">
+
+<div class="text">
+
+Esta história foi inspirada em acontecimentos reais.
+
+</div>
+
+</div>
+
+`;
+
+},1200);
+
+}
